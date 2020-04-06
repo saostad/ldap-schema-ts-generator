be4f8e21a717a81e4db5b85294e328797309c7c5
Ostad <SOstad@kajimausa.com>
2020-04-06 13:58:47 -0400

    [feat] more info in generated interface

--- src/helpers/generateText.ts
+++ src/helpers/generateText.ts
@@ -17,17 +17,22 @@ export function generateClassInterface({
   data,
 }: GenerateClassInterfaceFnInput): string {
   const result = `
-  /** object class: ${data.ldapName}
-   * 
-  * child of class: ${data.parentClass}
-  * 
-  * dn: ${data.originalClassFields.dn}
-  */
+  /**  - object class: ${data.ldapName}
+   *  - child of class: ${data.parentClass}
+   *  - dn: ${data.originalClassFields.dn}
+  */  
   interface ${pascalCase(data.className)} {
     ${arrayToLines(
       data.attributes &&
         data.attributes.map((el) => {
-          return `${el.systemOnly ? "readonly" : ""} "${el.lDAPDisplayName}" ${
+          return `
+          /**  - attributeSyntax: ${el.attributeSyntax} 
+           *   - attributeID: ${el.attributeID} 
+           *   - adminDisplayName: ${el.adminDisplayName} 
+           *   - adminDescription: ${el.adminDescription}
+           *   - dn: ${el.dn} 
+          */
+          ${el.systemOnly ? "readonly" : ""} "${el.lDAPDisplayName}" ${
             el.isRequired ? "" : "?"
           }: ${typeMapper(el.attributeSyntax)} ${
             el.isSingleValued ? "" : "[]"
